1039d52bb9d74eeed05bf7fbbad9e4092041284f
Aryan Singh Thapa <singh.deva2015@gmail.com>
2026-06-06 22:16:08 +0530

    Import axiosConfig in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './axiosConfig';
 import './index.css';
 import App from './App';
-import './axiosConfig';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
